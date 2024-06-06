b3ea6b036cb9be6914ac64c856f7b2ae4824bc82
Matheus Alexandre <matheusasj@live.com>
2024-06-06 00:21:47 -0300

    feat: added the sum of coins as result

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from ultralytics import YOLO
 import cv2
 import cvzone
-import math
 import torch
 
 torch.backends.cudnn.benchmark = True
@@ -19,9 +18,18 @@ resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 # Model initialization and inferation
 model = YOLO("yolov8s-brl-coin.pt")
-results = model(resized)  # return a list of Results objects
+prediction = model(resized)  # return a list of Results objects
 
-for r in results:
+results = 0
+coin_dict = {
+  0: 0.1,
+  1: 1.,
+  2: .25,
+  3: .5,
+  4: .05
+} 
+
+for r in prediction:
   for box in r.boxes:
     # Extracting th bounding box
     x, y, x1, y1 = box.xyxy[0]
@@ -29,10 +37,12 @@ for r in results:
     w, h = x1 - x, y1 - y
 
     # Model confidence
-    conf = math.ceil((box.conf[0]*100)/100)
+    conf = box.conf[0]
     # Naming classes
     cls = box.cls[0]
     current_class = model.names[int(cls)]
+
+    results += coin_dict[int(cls)]
     
     # Showing detection annotations
     cvzone.cornerRect(
@@ -44,14 +54,22 @@ for r in results:
 
     img, _ = cvzone.putTextRect(
       resized, 
-      text=f"{current_class} {conf}", 
+      text=f"{current_class} {conf:.2%}", 
       pos=(max(0, x), max(35, y)), 
-      scale=.25,
+      scale=.65,
       thickness=1,
       font=cv2.FONT_HERSHEY_SIMPLEX,
       offset=2
     )
-
+img, _ = cvzone.putTextRect(
+  resized, 
+  text=f"Total R$ {results:.3}", 
+  pos=(0, 25), 
+  scale=.65,
+  thickness=1,
+  font=cv2.FONT_HERSHEY_SIMPLEX,
+  offset=2
+)
 # Priting results and wating to exit
 cv2.imshow("Coin counter", img)
 cv2.waitKey(0)
